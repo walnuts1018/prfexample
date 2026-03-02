948dc6b418896b336de4d5e98a41fcfb2472a366
walnuts1018 <r.juglans.1018@gmail.com>
2026-03-03 01:19:24 +0900

    fix: format TODO comment for consistency in sha256_cert_fingerprints
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- server/router/handler/wellknown.go
+++ server/router/handler/wellknown.go
@@ -13,7 +13,7 @@ func (h Handler) AssertLinks(c *echo.Context) error {
 				"namespace":    "android_app",
 				"package_name": "dev.walnuts.test.prf_example",
 				"sha256_cert_fingerprints": []string{
-					"95:01:CA:38:FD:57:9B:D7:DD:21:2F:D1:F1:16:E0:76:4B:48:C5:36:AE:71:67:6A:2C:56:92:7B:B4:09:4D:63", //TODO: ~/.android/debug.keystore
+					"95:01:CA:38:FD:57:9B:D7:DD:21:2F:D1:F1:16:E0:76:4B:48:C5:36:AE:71:67:6A:2C:56:92:7B:B4:09:4D:63", // TODO: ~/.android/debug.keystore
 				},
 			},
 		},
