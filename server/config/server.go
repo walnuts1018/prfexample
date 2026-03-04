d4669a9b866377213727a93ea65f7075132ca2c3
walnuts1018 <r.juglans.1018@gmail.com>
2026-03-04 19:13:02 +0900

    fix: remove unnecessary blank line in Server struct definition
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- server/config/server.go
+++ server/config/server.go
@@ -6,5 +6,4 @@ type Server struct {
 	Port              int      `env:"PORT" envDefault:"8080" validate:"gte=1,lte=65535"`
 	Origin            *url.URL `env:"ORIGIN" envDefault:"https://prfexample.walnuts.dev"`
 	AdditionalOrigins []string `env:"ADDITIONAL_ORIGINS" envSeparator:","`
-	
 }
