40b8adf26cc8252052f31876efeb84d901bcd0a3
walnuts1018 <r.juglans.1018@gmail.com>
2026-03-05 16:52:03 +0900

    Remove comment about dynamic color availability in PRFExampleTheme function
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- app/src/main/java/dev/walnuts/test/prf_example/ui/theme/Theme.kt
+++ app/src/main/java/dev/walnuts/test/prf_example/ui/theme/Theme.kt
@@ -34,7 +34,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun PRFExampleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
