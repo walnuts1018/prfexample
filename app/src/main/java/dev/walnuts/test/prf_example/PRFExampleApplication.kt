d8f732f5b94a021c5711bcc78d8f172b5b9e8d7c
Walnuts <r.juglans.1018@gmail.com>
2026-03-05 16:42:27 +0900

    Revert "Fix ViewModels recreated on every configuration change"

--- app/src/main/java/dev/walnuts/test/prf_example/PRFExampleApplication.kt
+++ app/src/main/java/dev/walnuts/test/prf_example/PRFExampleApplication.kt
@@ -1,15 +1,9 @@
 package dev.walnuts.test.prf_example
 
 import android.app.Application
-import androidx.credentials.CredentialManager
-import dev.walnuts.test.prf_example.api.ApiClient
 import timber.log.Timber
 
 class PRFExampleApplication : Application() {
-    val credentialManager: CredentialManager by lazy { CredentialManager.create(this) }
-    val apiClient: ApiClient by lazy { ApiClient(baseUrl = "https://prfexample.walnuts.dev") }
-    val repository: PasskeyRepository by lazy { PasskeyRepository(credentialManager, apiClient, this) }
-
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
